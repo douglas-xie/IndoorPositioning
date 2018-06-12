2c5f6dd597e8ca4d4c709003a9d02c1653118a96
TobiasXie <sun_xietao@163.com>
2018-06-12 10:45:38 +0800

    remove validation tags

--- AIS_ELM_ILS/Demo1.m
+++ AIS_ELM_ILS/Demo1.m
@@ -7,10 +7,10 @@ Pd0 = 31.7;%单位db，测量后的值
 PT = 0;%单位dbm,发射功率0
 n = 2.2;%路径损耗指数
 sigma= 3;%增加的0均值高斯过程的标准差
-N = 100;%收集了100组数据
+N = 1000;%收集了100组数据
 %% The setting of Reader, Reference tag and Test tag
 % 在空间的角落放置4个阅读器
-NumberOfReader =  4;
+NumberofReader =  4;
 PosReader = [-0.5,-0.5;-0.5,10.5;10.5,-0.5;10.5,10.5];
 NumberofTag = 121;    % 参考标签的数量
 PosTag = zeros(NumberofTag, 2);
@@ -21,106 +21,96 @@ for iRow = 1:11
       PosTag((iRow-1)*11+jCol,:) = [(jCol-1) (iRow-1)]; 
    end
 end
-% 随机生成121个验证标签
-% NumberofValTag = 121;    % 验证标签的数量
-% PosValTag = 10 * rand(NumberofValTag, 2);
+% 使用参考标签计算验证误差
 NumberofValTag = 121;
 PosValTag = PosTag;
 % 在空间中随机生成1000个测试标签，用于测试定位效果。
 NumberofTestTag = 1000;     % 测试标签的数量
 PosTestTag = 10 * rand(NumberofTestTag, 2);    % 随机产生测试标签的位置
 
-PR = zeros(NumberofTag, NumberOfReader, N);        % 参考标签RSSI值
-PR_Val = zeros(NumberofValTag, NumberOfReader, N); % 验证标签RSSI值 
-PR_Test = zeros(NumberofTestTag, NumberOfReader, N); % 测试标签RSSI值
+PR = zeros(NumberofTag, NumberofReader, N);        % 参考标签RSSI值
+PR_Val = zeros(NumberofValTag, NumberofReader, N); % 验证标签RSSI值 
+PR_Test = zeros(NumberofTestTag, NumberofReader, N); % 测试标签RSSI值
 
 %% Calculate the distances
   % 参考标签与阅读器之间的距离
-[d_RT] = calDistance(NumberofTag, NumberOfReader, PosTag, PosReader);
-% 计算验证标签到阅读器的距离
-[d_RTV]=calDistance(NumberofValTag, NumberOfReader, PosValTag, PosReader);
+[d_RT] = calDistance(NumberofTag, NumberofReader, PosTag, PosReader);
  % 测试标签与阅读器之间的距离
-[d_RTT]=calDistance(NumberofTestTag, NumberOfReader, PosTestTag, PosReader);
+[d_RTT]=calDistance(NumberofTestTag, NumberofReader, PosTestTag, PosReader);
 
 %% Experiment setting
 
-Iters = 10;  % 最大迭代次数
-Error1 = zeros(Iters, 4); % 每次迭代的训练误差
-Error2 = zeros(Iters,4);  % 每次迭代的测试误差
+Iters = 1;  % 最大迭代次数
+Error1 = zeros(Iters, NumberofReader); % 每次迭代的训练误差
+Error2 = zeros(Iters,NumberofReader);  % 每次迭代的测试误差
 trainError = [];
 testError = [];
 
-OutputofTrain = zeros(NumberofTag,2,Iters);
-OutputofTest = zeros(NumberofTestTag,2,Iters);
+% 训练输出和测试输出结果
+OutputofTrain = zeros(NumberofTag,2);
+OutputofTest = zeros(NumberofTestTag,2);
 %% Start running
-for n = 1:10
+for n = 10
     fprintf('The loss path constant is: %d\n', n);
 for iter = 1:Iters
 %============================计算RSSI值============================%
     Para = [PT, Pd0, d0, n, sigma, N]; % RSSI参数设置
-    [PR, PR_Val, PR_Test] = calPR(NumberofTag, NumberofValTag, ...
-             NumberofTestTag, NumberOfReader, d_RT, d_RTV, d_RTT,Para);
+    [PR, PR_Test] = calPR(NumberofTag, NumberofTestTag, NumberofReader, ...
+        d_RT, d_RTT,Para);
 %==========================数据预处理===============================%
 %================高斯滤波===================%
-    [PRFilter]=GaussianFilter(PR,NumberofTag,NumberOfReader,N);
-    [PRValFilter]=GaussianFilter(PR_Val,NumberofValTag,NumberOfReader,N);
-    [PRTestFilter]=GaussianFilter(PR_Test,NumberofTestTag,NumberOfReader,N);
+    [PRFilter]=GaussianFilter(PR,NumberofTag,NumberofReader,N);
+    [PRTestFilter]=GaussianFilter(PR_Test,NumberofTestTag,NumberofReader,N);
     % 归一化处理
-    PRGY = [PRFilter;PRValFilter;PRTestFilter];
+    PRGY = [PRFilter; PRTestFilter];
     [PRGY, PRGYMIN, PRGYMAX] = normalPR(PRGY);
     [GYrow,GYcol] = size(PRGY);
     % 划分训练集、验证集和测试集
     TrainInput = PRGY(1:NumberofTag,:);
-%     ValInput = PRGY(NumberofTag+1:NumberofTag+NumberofValTag,:);
     ValInput = TrainInput;
-    TestInput = PRGY(NumberofTag + NumberofValTag+1:end,:);
+    TestInput = PRGY(NumberofTag + 1:end,:);
     
     %% ELM
     % Initialize the parameters of ELM
-    NumberofHidden = 28;    % 隐层节点个数
-    epsilon_init = sqrt(6)./sqrt(4+NumberofHidden);
-    InputWeight_init = 2*rand(NumberofHidden, 4)*epsilon_init-epsilon_init;% 初始化输入权重
+    NumberofHidden = 10;    % 隐层节点个数
+    epsilon_init = sqrt(6)./sqrt(4+NumberofHidden); % 设置合理的阈值 
+    InputWeight_init = 2*rand(NumberofHidden, NumberofReader)*epsilon_init-epsilon_init;% 初始化输入权重
     HiddenBias_init = 2*rand(NumberofHidden,1)*epsilon_init-epsilon_init; % 初始化隐层神经元偏置
     % 直接使用ELM
-    [OutputofTrain(:,:,iter),OutputofTest(:,:,iter),trainTime,testTime]= ...
+    [OutputofTrain,OutputofTest,trainTime,testTime]= ...
         ELM(PosTag,TrainInput,TestInput,NumberofHidden,...
         InputWeight_init,HiddenBias_init);
     % 计算定位误差
-    Error1(iter, 1) = calLoss(NumberofTag,OutputofTrain(:,:,iter), PosTag); % 训练误差
+    Error1(iter, 1) = calLoss(NumberofTag,OutputofTrain, PosTag); % 训练误差
     [Error2(iter, 1),max_error, min_error] = ...
-        calLoss(NumberofTestTag,OutputofTest(:,:,iter), PosTestTag);
+        calLoss(NumberofTestTag,OutputofTest, PosTestTag);
     %% 使用免疫算法优化
-    NumberofHidden = 28;
+    NumberofHidden = 10;
     [InputWeight_AIS,HiddenBias_AIS]=AIS_ELM(InputWeight_init,HiddenBias_init,...
         NumberofHidden, NumberofTag, PosTag,TrainInput, NumberofValTag, ...
         PosValTag, ValInput);
        % 使用 ELM
-    [OutputofTrain(:,:,iter),OutputofTest(:,:,iter),trainTime,testTime]= ...
+    [OutputofTrain,OutputofTest]= ...
         ELM(PosTag,TrainInput,TestInput,NumberofHidden,InputWeight_AIS,HiddenBias_AIS);
     % 计算测试定位误差
-    Error1(iter, 2) = calLoss(NumberofTag,OutputofTrain(:,:,iter), PosTag); % 训练误差
-    Error2(iter, 2) = calLoss(NumberofTestTag,OutputofTest(:,:,iter), PosTestTag);
+    Error1(iter, 2) = calLoss(NumberofTag,OutputofTrain, PosTag); % 训练误差
+    Error2(iter, 2) = calLoss(NumberofTestTag,OutputofTest, PosTestTag);
     %% 使用PSO优化
     [InputWeight_PSO, HiddenBias_PSO] = ...
         PSO_ELM(InputWeight_init, HiddenBias_init, NumberofHidden, ...
         PosTag,TrainInput, PosValTag, ValInput);
     % 使用 ELM
-    [OutputofTrain(:,:,iter),OutputofTest(:,:,iter),trainTime,testTime]= ...
+    [OutputofTrain,OutputofTest]= ...
         ELM(PosTag,TrainInput,TestInput,NumberofHidden,InputWeight_PSO,HiddenBias_PSO);
     % 计算测试定位误差
-    Error1(iter, 3) = calLoss(NumberofTag,OutputofTrain(:,:,iter), PosTag); % 训练误差
-    Error2(iter, 3) = calLoss(NumberofTestTag,OutputofTest(:,:,iter), PosTestTag);
+    Error1(iter, 3) = calLoss(NumberofTag,OutputofTrain, PosTag); % 训练误差
+    Error2(iter, 3) = calLoss(NumberofTestTag,OutputofTest, PosTestTag);
     
     %% 使用RBF
     [error]=RBFILS(PosTag,TrainInput,TestInput,NumberofHidden,PosTestTag);
     Error1(iter, 4) = error(1,1);
     Error2(iter, 4) = error(1,2);
 end
-    trainError = [trainError; mean(Error1, 1)];
-    testError = [testError; mean(Error2, 1)]   % 平均误差
+    trainError = [trainError; mean(Error1, 1)];  % 平均训练误差
+    testError = [testError; mean(Error2, 1)]   % 平均测试误差
 end
-columns = {'ELM', 'AIS-ELM', 'PSO-ELM', 'RBF'};
-xlswrite('Results\20180611\output(N=100).xls', columns, 'TrainError', 'A1');
-xlswrite('Results\20180611\output(N=100).xls', trainError, 'TrainError', 'A2');
-xlswrite('Results\20180611\output(N=100).xls', columns, 'TestError', 'A1');
-xlswrite('Results\20180611\output(N=100).xls', testError, 'TestError', 'A2');
--- AIS_ELM_ILS/calPR.m
+++ AIS_ELM_ILS/calPR.m
@@ -1,5 +1,5 @@
-function [PR, PR_Val, PR_Test] = calPR(NumberofTag, NumberofValTag, ...
-              NumberofTestTag, NumberofReader, d_RT, d_RTV, d_RTT, Para)
+function [PR, PR_Test] = calPR(NumberofTag, NumberofTestTag, ...
+    NumberofReader, d_RT, d_RTT, Para)
 PT = Para(1);
 Pd0 = Para(2); 
 d0 = Para(3);
@@ -7,15 +7,11 @@ n = Para(4);
 sigma = Para(5);
 N = Para(6);
 PR = zeros(NumberofTag, NumberofReader, N);
-PR_Val = zeros(NumberofValTag, NumberofReader, N);
 PR_Test = zeros(NumberofTestTag, NumberofReader, N);
 for j = 1:N
-    AddGauss=sigma * randn(NumberofTag+NumberofValTag+NumberofTestTag,...
-        NumberofReader);
+    AddGauss=sigma * randn(NumberofTag+NumberofTestTag, NumberofReader);
     PR(:,:,j)=PT-(Pd0+10.*n.*log10(d_RT./d0)+AddGauss(1:NumberofTag,:));
-    PR_Val(:,:,j) = PT-(Pd0+10.*n.*log10(d_RTV./d0)+...
-        AddGauss(NumberofTag+1:NumberofTag+NumberofValTag,:));
     PR_Test(:,:,j) = PT - (Pd0 + 10.*n.*log10(d_RTT./d0)+...
-        AddGauss(NumberofValTag+NumberofTag+1:end,:));
+        AddGauss(NumberofTag+1:end,:));
 end
 end
